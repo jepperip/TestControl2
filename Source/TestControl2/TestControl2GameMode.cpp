33d7cce8d1ab074bc11a87a40be3c0bf40c7eecb
Jesper Sahlin <jesper.sahlin@gmail.com>
2015-03-03 12:32:01 +0100

    hej

---
+++ Source/TestControl2/TestControl2GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestControl2.h"
+#include "TestControl2GameMode.h"
+
+
+
+
---
+++ Source/TestControl2/TestControl2GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TestControl2GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCONTROL2_API ATestControl2GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
